47722d62f5984fa5f2ebcc5c8384337dcc914ed9
unifolio0 <boho82ha@gmail.com>
2025-10-06 15:21:23 +0900

    test: @EnableAsync 명시

--- aop-proxy/src/main/java/nextstep/Application.java
+++ aop-proxy/src/main/java/nextstep/Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
